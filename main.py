e6d84111345a0e5769125ea89877db23563f8b70
WH1NER7 <danil-2208@mail.ru>
2023-12-14 20:01:25 +0500

    updated autoanswer for myk and bonasita

--- main.py
+++ main.py
@@ -4,15 +4,51 @@ import requests
 import json
 import random
 import logging
+import pymorphy3
 
 
-def log_feedback_response(response_text, feedback, feedback_text):
+def detect_name_and_gender(name):
+    first_name = name.split(" ")[0]
+    morph = pymorphy3.MorphAnalyzer(lang='ru')
+    parsed_name = morph.parse(first_name)
+
+    for result in parsed_name:
+        if 'Name' in result.tag and "femn" in result.tag:
+            return 'female'
+        elif 'Name' in result.tag and "masc" in result.tag:
+            return 'male'
+        else:
+            return 'female'
+    return 'female'
+
+
+def get_feedback_text_category(has_photo, has_user_name, sex, valuation):
+    if valuation < 5:
+        return "negative"
+
+    categories = {
+        (True, True, 'male'): "name_image_male",
+        (True, True, 'female'): "name_image_female",
+        (True, False, 'female'): "anon_images_female",
+        (False, True, 'male'): "no_photos_name_male",
+        (False, True, 'female'): "no_photos_name_female",
+        (False, False, 'male'): "anon_no_photo_male",
+        (False, False, 'female'): "anon_no_photo_female",
+    }
+
+    return categories.get((has_photo, has_user_name, sex), "no_photos_name_female")
+
+
+def log_feedback_response(response_text, feedback):
     timestamp = datetime.datetime.now().isoformat()
     log_entry = {
-        "feedback": feedback,
+        "valuation": feedback.get("productValuation"),
+        "feedback_id": feedback.get("id"),
+        "nmId": feedback.get('productDetails').get("nmId"),
         "response_text": response_text,
         "timestamp": timestamp,
-        "feedback_text": feedback_text
+        "feedback_text": feedback.get("text"),
+        "user_name": feedback.get("userName")
     }
 
     with open("feedback_log.json", "a", encoding="utf-8") as log_file:
@@ -27,18 +63,17 @@ def get_token_evn(company):
         return ''
 
 
-def get_feedback_text(company_name, user_name, category):
-    json_file_path = 'texts.json'
+def get_feedback_text(company, user_name, category):
+    json_file_path = f'texts_{company}.json'
 
     with open(json_file_path, 'r', encoding='utf-8') as json_file:
         data = json.load(json_file)
 
     random_text = random.choice(data[category])
     updated_text = random_text.replace("(ИМЯ)", user_name)
-    updated_text = updated_text.replace("MissYourKiss", company_name)
+    updated_text = updated_text.replace("MissYourKiss", company)
 
     return updated_text
-    # print(random_text)
 
 
 def get_unanswered_feedbacks(company):
@@ -72,65 +107,28 @@ def answer_to_feedback(feedback_id, company, feedback_text, feedback):
     response = requests.patch(url=url, json=body, headers=headers)
     response_text = response.json()
     logging.info(f"Response for feedback {feedback_id}: {response_text}")
-    log_feedback_response(response_text, feedback, feedback_text)
+    log_feedback_response(response_text, feedback)
     print(feedback_text, response_text)
 
 
-def main():
-    # for company in ['Bonasita', 'MissYourKiss']:
-    for company in ['Bonasita']:
+def answer_to_feedbacks_all():
+    for company in ["MissYourKiss", "Bonasita"]:
         feedback_pool = get_unanswered_feedbacks(company)
         for feedback in feedback_pool:
-            print(feedback)
-            if feedback['productValuation'] == 5:
-                text = ''
+            if feedback.get('productDetails').get("nmId") not in [131619917, 166281374, 150623763, 135933841, 171221030, 143418102, 182849819, 166779160, 151137559, 175757013, 150623767, 150623771]:
                 feedback_id = feedback.get('id')
                 user_name = feedback.get('userName')
                 has_user_name = bool(feedback.get('userName'))
                 has_photo = bool(feedback.get('photoLinks'))
-                if has_photo and has_user_name:
-                    text = get_feedback_text(company, user_name, "name_image")
-                elif has_photo and not has_user_name:
-                    text = get_feedback_text(company, user_name, "anon_images")
-                elif not has_photo and has_user_name:
-                    text = get_feedback_text(company, user_name, "no_photos_name")
-                elif not has_photo and not has_user_name:
-                    text = get_feedback_text(company, user_name, "anon_no_photo")
+                sex = detect_name_and_gender(user_name)
 
-                if text:
-                    pass
-                    answer_to_feedback(feedback_id, company, text, feedback)
+                text_category = get_feedback_text_category(has_photo, has_user_name, sex, feedback.get("productValuation"))
+                text = get_feedback_text(company, user_name, text_category)
 
-
-def answer_to_empty_feedbacks_myk():
-    for company in ['MissYourKiss']:
-        feedback_pool = get_unanswered_feedbacks(company)
-        for feedback in feedback_pool:
-            # print(feedback)
-            if feedback['productValuation'] == 5 and feedback['text'] == '':
-                text = ''
-                feedback_id = feedback.get('id')
-                user_name = feedback.get('userName')
-                has_user_name = bool(feedback.get('userName'))
-                has_photo = bool(feedback.get('photoLinks'))
-                if has_photo and has_user_name:
-                    text = get_feedback_text(company, user_name, "name_image")
-                elif has_photo and not has_user_name:
-                    text = get_feedback_text(company, user_name, "anon_images")
-                elif not has_photo and has_user_name:
-                    text = get_feedback_text(company, user_name, "no_photos_name")
-                elif not has_photo and not has_user_name:
-                    text = get_feedback_text(company, user_name, "anon_no_photo")
-
-                if text:
-                    pass
-                    answer_to_feedback(feedback_id, company, text, feedback)
+                answer_to_feedback(feedback_id, company, text, feedback)
 
 
 if __name__ == '__main__':
-    answer_to_empty_feedbacks_myk()
-    # print(get_feedback_text('Bonasita', '', "anon_images"))
-    # print(get_feedback_text('Bonasita', '', "anon_no_photo"))
-    # print(get_feedback_text('Bonasita', 'ывы', "name_image"))
-    # print(get_feedback_text('Bonasita', 'ывы', "no_photos_name"))
-    main()
+    answer_to_feedbacks_all()
+
+
